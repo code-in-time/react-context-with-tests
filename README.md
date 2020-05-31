9c9fe25d62a6bd898c0dd3b1fc39cfad1b63f634
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-05-31 10:52:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Notes
+
+- This is a simple example of writing a very simple react app counter with tests.
+
+```
+- npm install
+- npm run start
+- npm run test
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function App() {
+
+  const [countVal, setCountVal] = useState(0)
+
+  return (
+    <div className="App" data-test="app-box">
+    </div>
+  );
+}
+
+export default App;
