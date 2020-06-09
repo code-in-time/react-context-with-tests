0e3828987f73656bf08ac78988cb76331ae9a167
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-06-09 19:11:52 +0200

    make simple

--- src/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
-test('that the app component exists', () => {
-      const wrapper = shallow(<App />)
-      expect(wrapper.find('[data-test="app-box"]').exists()).toBeTruthy()
-})
--- src/LanguageSelect.tsx
+++ src/LanguageSelect.tsx
@@ -1,40 +1,13 @@
 import React, { useContext } from 'react';
-import { LanguageContext, actions } from './utils/LanguageHook';
-import { languages, ILang } from './utils/languageData';
+import { LanguageContext } from './utils/LanguageHook';
 
 
 function LanguageSelect() {
     const pValue: any = useContext(LanguageContext);
-    const selectLanguageOnClick = (key: string) => {
-        pValue.dispatch({ type: actions.LANG_SET, payload: key.toString() })
-    }
-
-    const buildButtons = (allLang: ILang) => {
-        let oButtons: any = []
-        const keys = Object.keys(allLang)
-        for (const key of keys) {
-            oButtons.push(
-                <button
-                    key={key}
-                    className={`btn-${key} ${key === pValue.lang ? 'selectedLang' : ''}`}
-                    onClick={() => {
-                        selectLanguageOnClick(key)
-                    }}
-                >{allLang[key].title}</button>)
-        }
-
-        return (
-            <div className="btnContainer">
-                {oButtons}
-            </ div>
-        )
-    }
 
     return (
         <div className="LanguageSelect" data-test="languageSelect-box">
             <div className="languageArea">{pValue.state.fullLang}</div>
-            {pValue.state.lang}
-            {buildButtons(languages)}
         </div>
     );
 }
