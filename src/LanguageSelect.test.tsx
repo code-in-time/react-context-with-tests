81f665a487818f2e475170d29e59f3d3b26c9c2d
code-in-time@hotmail.com <andrew.crawford.za@gmail.com>
2020-06-09 19:36:59 +0200

    fix test: Add mock in the correct place

--- src/LanguageSelect.test.tsx
+++ src/LanguageSelect.test.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import LanguageSelect from './LanguageSelect';
 
+jest.mock('react', () => {
+  const ActualReact = require.requireActual('react');
+  return {
+    ...ActualReact,
+    useContext: () => ({ state: { fullLang: 'mocked context' } }),
+  };
+});
+
 test.only('that the LanguageSelect component exists', () => {
 
-    jest.mock('react', () => {
-        const ActualReact = require.requireActual('react');
-        return {
-          ...ActualReact,
-          useContext: () => ({ state: { fullLang: 'mocked context' } }),
-      };
-    });
-    const wrapper = shallow(<LanguageSelect />)
-    let s = 1;
-    expect(wrapper.find('[data-test="languageSelect-box"]').exists()).toBeTruthy()
-    wrapper.unmount();
+
+  const wrapper = shallow(<LanguageSelect />)
+  expect(wrapper.find('[data-test="languageSelect-box"]').exists()).toBeTruthy()
+  wrapper.unmount();
 })
 
 // test('that the language text exists', () => {
